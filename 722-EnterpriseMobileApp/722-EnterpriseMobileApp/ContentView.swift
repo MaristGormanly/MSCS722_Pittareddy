5cb184516d856f3f9000569c5d73bc7a51a45e0d
banuprakashreddypitta <123662271+banuprakashreddypitta@users.noreply.github.com>
2023-01-29 11:14:47 +0530

    Initial Commit
    First commit on project creation.

---
+++ 722-EnterpriseMobileApp/722-EnterpriseMobileApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  722-EnterpriseMobileApp
+//
+//  Created by Banu Prakash Reddy on 29/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding(.top,100)
+        Spacer()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 722-EnterpriseMobileApp/MSCS722-Pittareddy/MSCS722-Pittareddy/MSCS722_PittareddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSCS722_PittareddyApp.swift
+//  MSCS722-Pittareddy
+//
+//  Created by Banu Prakash Reddy on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MSCS722_PittareddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
